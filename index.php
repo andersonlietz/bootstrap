2300e3401b5cb89ea2c982832962f261a1bed94a
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-09-28 16:11:47 -0300

    Erste Commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap</title>
+</head>
+<body>
+<div class="text-center">
+ <h1 class="display-3">Cocina del Jef</h1>
+ <p>Delivery de comida brasileira em São Paulo</p>
+<p>Para imprimir nossas receitas tecle 
+    <kbd>Ctrl+P</kbd>
+</p>
+ <mark>Peça pelo app</mark>
+</div>
+<div class="container">
+
+<div class="row">
+
+</div>
+<div class="col-sm-4">
+    <h3>Recomendação</h3>
+    <img src="produto.jpg" class=".img-thumbnail" >
+    <br><b>Nome do prato</b>
+    <p>Descrição do prato</p>
+ </div>
+<div class="col-sm-4">
+<h3>Recomendação</h3>
+    <img src="produto.jpg" class=".img-thumbnail" >
+    <br><b>Nome do prato</b>
+    <p>Descrição do prato</p>
+</div>
+ <div class="col-sm-4">
+<h3>Recomendação</h3>
+    <img src="produto.jpg" class=".img-thumbnail" >
+    <br><b>Nome do prato</b>
+    <p>Descrição do prato</p>
+ </div>
+<table class=".table-light">
+<tr class=".table-light">
+            <td>ALIMENTOS</td>
+            <td>PROTEINAS</td>
+            <td>CARBOIDRATOS</td>
+        </tr>
+        <tr class=".table-light">
+            <td>Alimento</td>
+            <td>Proteinas</td>
+            <td>Carboidratos</td>
+        </tr>
+        <tr class=".table-light">
+            <td>Alimento</td>
+            <td>Proteinas</td>
+            <td>Carboidratos</td>
+        </tr>
+        <tr class=".table-light">
+            <td>Alimento</td>
+            <td>Proteinas</td>
+            <td>Carboidratos</td>
+        </tr>
+
+</table>
+
+
+
+
+     </div>
+</body>
+</html>
